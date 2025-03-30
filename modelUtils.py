221fe72dd77251903935e165e2c3dd10bfd485f9
George Wang <gw103@duke.edu>
2025-03-30 13:55:10 -0400

    Model written

--- modelUtils.py
+++ modelUtils.py
@@ -3,21 +3,44 @@ import torch.nn as nn
 import torchvision
 
 
-class simpleClassifer(nn.Module):
+class Classifier(nn.Module):
     def __init__(self):
-        super(simpleClassifer, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        super(Classifier, self).__init__()
+        self.model = torchvision.models.resnet18(pretrained=True)
+        self.model.fc = nn.Linear(512, 14)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return
+        return self.model(x)
+    
+class Regressor(nn.Module):
+    def __init__(self, classifer_dir=None, classifier=None):
+        super(Regressor, self).__init__()
+        self.classifier = load_state_dict(torch.load(classifer_dir))
+        self.classifier.eval() 
+        #Can adjust here in the future 
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        self.regressor_fc = nn.Sequential(
+            nn.Linear(14 + 10, 128), # Input: 14-dimensional vector + 10-dimensional vector
+            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Linear(512, 14)  # Output: 14-dimensional vector
+        )
+        
+
+    def forward(self, x, classifier_output):
+        # Concatenate classifier output with image features
+        raw = x
+        x = self.conv_layers(raw)
+        res = self.classifier(raw)
+        x = x.view(x.size(0), -1)
+        x = torch.cat([x, res], dim=1)
+        x = self.regressor_fc(x)
+        return x
