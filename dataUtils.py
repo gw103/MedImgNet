6e7326ffa2d5e9d2a7dc1bfb79b09d567521e9b0
George Wang <gw103@duke.edu>
2025-05-03 15:47:32 -0400

    fix minor bug

--- dataUtils.py
+++ dataUtils.py
@@ -163,7 +163,7 @@ def get_last_image_index():
 
 
 class ImageLabelDataset(Dataset):
-    def __init__(self, csv_file='../datasets/nih-chest-xrays/Data_Entry_filtered.csv, directories=None, transform=None):
+    def __init__(self, csv_file='../datasets/nih-chest-xrays/Data_Entry_filtered.csv', directories=None, transform=None):
         """
         Args:
             csv_file (string): Local path to the CSV file with image indices and labels.
