fe80f5df9396c22f8a0ff7a975fa54d56b94b8da
George Wang <gw103@duke.edu>
2025-05-03 16:17:07 -0400

    fixed a minor bug

--- get_csv_info.py
+++ get_csv_info.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from collections import Counter
 
-df = pd.read_csv("'../datasets/nih-chest-xrays/Data_Entry_filtered.csv'")
+df = pd.read_csv("../datasets/nih-chest-xrays/Data_Entry_filtered.csv")
 
 # Split labels and flatten the list
 all_labels = df['Finding Labels'].str.split('|').explode().str.strip()
